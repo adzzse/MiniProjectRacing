11911ce62899be33e494ecfae271b6e3a88ab5d3
Adzzse <noik0vshack@gmail.com>
2025-02-10 23:32:47 +0700

    Initial commit(real)

---
+++ app/src/main/java/com/example/miniprojectracing/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.miniprojectracing;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Chuyển thẳng đến LoginActivity
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+
+        // Kết thúc MainActivity sau khi chuyển
+        finish();
+    }
+}
